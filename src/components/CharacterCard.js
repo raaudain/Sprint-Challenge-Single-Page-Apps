064f3575050dbfe3165bfe00530c8a8bde4fabab
Ramon Audain <raudain123@gmail.com>
2019-09-20 08:47:28 -0700

    character info displaying

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
+import {Col, Card, CardBody, CardTitle, CardText, CardImg} from "reactstrap"
+import jssPluginPropsSort from "jss-plugin-props-sort";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <Col md="4" key={props.id}>
+      <Card>
+        <CardBody>
+          <CardImg src={props.image} />
+          <CardTitle tag="h2">{props.name}</CardTitle>
+          <CardText className="text-left">Status: {props.status}</CardText>
+          <CardText className="text-left">Species: {props.species}</CardText>
+          <CardText className="text-left">Gender: {props.gender}</CardText>
+          
+        </CardBody>
+      </Card>
+    </Col>
+  )
 }
